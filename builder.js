ca3d28666530477706913f9a57b8ebb1b4f4be23
Rich Abdill <richard.abdill@gmail.com>
2017-11-27 21:54:46 -0600

    Filtering based on person

--- builder.js
+++ builder.js
@@ -1,14 +1,28 @@
-function buildPage() {
+function buildContent(filter) {
     var content = "<ul>";
     for(var i=0, entry; entry=entries[i]; i++){
-        content += "<li"
-        if(entry.emphasis) {
-            content += ` class="emphasize"`
+        var toPrint = true;
+        if(filter) {
+            toPrint = false;
+            if(entry.people) {
+                for(var j=0, person; person=entry.people[j]; j++) {
+                    if(person == filter) {
+                        toPrint = true;
+                        break;
+                    }
+                }
+            }
         }
-        content += `><strong>` + entry.display_date + `:</strong> ` + entry.event;
-
-        if(entry.image) {
-            content += `<br><center><img class="img-fluid" width="` + (entry.image.width || 100) + `%" src="images/` + entry.image.filename + `"></center>`
+        if(toPrint) {
+            content += "<li"
+            if(entry.emphasis) {
+                content += ` class="emphasize"`
+            }
+            content += `><strong>` + entry.display_date + `:</strong> ` + entry.event;
+    
+            if(entry.image) {
+                content += `<br><center><a href="images/` + entry.image.filename + `" target="_blank"><img class="img-fluid rounded" width="` + (entry.image.width || 100) + `%" src="images/` + entry.image.filename + `"></a></center>`
+            }
         }
     }
     content += "</ul>"
@@ -16,5 +30,29 @@ function buildPage() {
     document.getElementById("title").innerHTML = pageTitle;
 }
 
+function buildTags() {
+    // build a duplicate-free list of all the people:
+    var people = [];
+    for(var i=0, entry; entry=entries[i]; i++){
+        if(entry.people) {
+            for(var j=0, person; person=entry.people[j]; j++) {
+                people.push(person);
+            }
+        }
+    }
+    var people = _.uniq(people);
+
+    // print out the list in the "tags" column
+    var tags = "";
+    for(var i=0, person; person=people[i]; i++) {
+        tags += `<span onclick="buildContent('` + person + `');">` + person + `</span><br>`;
+    }
+    document.getElementById("tags").innerHTML = tags;
+}
+
+function buildPage() {
+    buildContent()
+    buildTags()
+}
 
 buildPage()
--- data.js
+++ data.js
@@ -2,33 +2,56 @@ var pageTitle = "Lacerra Family Timeline";
 
 var entries = [
     {
-        "sort_date": 1856,
-        "display_date": `1856 or 1860, February 12`,
-        "event": `Felice "Grandpa" Lacerra born in Italy`
+        sort_date: 1856,
+        display_date: `1856 or 1860, February 12`,
+        event: `Felice "Grandpa" Lacerra born in Italy`,
+        people: [
+            "Felice Lacerra"
+        ]
     },
     {
-        "sort_date": 1863,
-        "display_date": `1863, July`,
-        "event": `Lucia "Grandma' Lacerra (nee Natale?) born in Italy`
+        sort_date: 1863,
+        display_date: `1863, July`,
+        event: `Lucia "Grandma' Lacerra (nee Natale?) born in Italy`,
+        people: [
+            "Lucia Lacerra"
+        ]
     },
     {
-        "sort_date": 1878,
-        "display_date": `1878, about`,
-        "event": `Felice and Lucia Lacerra married in Italy`
+        sort_date: 1878,
+        display_date: `1878, about`,
+        event: `Felice and Lucia Lacerra married in Italy`,
+        people: [
+            "Felice Lacerra", "Lucia Lacerra"
+        ]
     },
     {
-        "sort_date": 1879,
-        "display_date": `1879, October 27`,
-        "event": `Felice and Lucia Lacerra arrive in New York Harbor through Castle Garden coming from Italy`,
-        "emphasis": true,
-        "image": {
-            "filename": "lacerra_manifest.png",
-            "width": 50,
+        sort_date: 1879,
+        display_date: `1879, October 27`,
+        event: `Felice and Lucia Lacerra arrive in New York Harbor through Castle Garden coming from Italy`,
+        emphasis: true,
+        image: {
+            filename: "lacerra_manifest.png",
+            width: 50,
         },
+        people: [
+            "Felice Lacerra", "Lucia Lacerra"
+        ]
     },
     {
-        "sort_date": 1879.1,
-        "display_date": `1879`,
-        "event": `Felice and Lucia Lacerra move into an apartment in Little Italy, New York at corner of Mott Street and Pell Street`
+        sort_date: 1879.1,
+        display_date: `1879`,
+        event: `Felice and Lucia Lacerra move into an apartment in Little Italy, New York at corner of Mott Street and Pell Street`,
+        people: [
+            "Felice Lacerra", "Lucia Lacerra"
+        ]
+    },
+    {
+        sort_date: 1883,
+        display_date: `1883 or 1893, March 31`,
+        event: `James "Poppy" Cherry born at Mott & Pell Street, New York, New York`,
+        people: [
+            "James Cherry"
+        ]
     },
 ]
