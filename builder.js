9840efc5ea217dd0e737059bf65b197f0e3cfff9
Rich Abdill <richard.abdill@gmail.com>
2017-11-27 21:06:16 -0600

    Pulling in Bootstrap

--- builder.js
+++ builder.js
@@ -1,10 +1,11 @@
 function buildPage() {
     var content = "<ul>";
     for(var i=0, entry; entry=entries[i]; i++){
-        content += "<li>" + entry.display_date + ": " + entry.event;
+        content += "<li><strong>" + entry.display_date + ":</strong> " + entry.event;
     }
     content += "</ul>"
     document.getElementById("content").innerHTML = content;
+    document.getElementById("title").innerHTML = pageTitle;
 }
 
 
